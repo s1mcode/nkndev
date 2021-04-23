0e398866a802da2e2afff8674258d4cd5d389eee
OakhillBarrera <82925026+OakhillBarrera@users.noreply.github.com>
2021-04-22 19:29:31 -0700

    Update nkn-commercial-install.sh

--- nkn-commercial-install.sh
+++ nkn-commercial-install.sh
@@ -8,8 +8,10 @@ NKN_LOG_DIR="$NKN_DIR/Log"
 # 格式错误 或者 地址错误 使用 脚本作者的地址
 # BENEFICIARY_ADDR=`cat /root/nkn_beneficiary_addr`
 
-# 改为 参数形式 如 ./nkn.sh NKNV4hbx3QfFQgrGbxzBMt7LeeDfQ3toBeMJ
+# 改为 参数形式 如 ./nkn.sh NKNV4hbx3QfFQgrGbxzBMt7LeeDfQ3toBeMJ http://34.99.99.99/ChainDB.tar.gz
 BENEFICIARY_ADDR=$1
+CHAINDB_PATH=$2
+
 
 if [ ${#BENEFICIARY_ADDR} != 36 ];then
     BENEFICIARY_ADDR="NKNV4hbx3QfFQgrGbxzBMt7LeeDfQ3toBeMJ"
@@ -21,14 +23,19 @@ echo $BENEFICIARY_ADDR
 #   mv $NKN_DIR/ChainDB /root/db_bakup/ChainDB
 # fi
 
-rm -rf $NKN_DIR
+rm -rf $NKN_DIR #将目录及以下之档案亦逐一删除。即使原档案属性设为唯读，亦直接删除，无需逐一确认。
 
-mkdir -p $NKN_COMMERCIAL_DIR
+mkdir -p $NKN_COMMERCIAL_DIR #创建目录
 
-step 1
-apt-get update -qq
+#step 1
+apt-get update -qq #重新获取软件包列表。不输出信息，错误除外。
 apt-get install -y unzip net-tools psmisc git htop nano haveged supervisor nginx
-step 1
+#step 1
+
+#step 1.2
+cd /root
+wget -O - CHAINDB_PATH -q | tar -xzf - 
+#step 1.2
 
 # step 2.1 (nkn config.mainnet.json)
 cd $NKN_DIR
