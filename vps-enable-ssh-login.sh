72ec186a3646fb9ece2377517bd8f5a6f5b1dbe1
OakhillBarrera <82925026+OakhillBarrera@users.noreply.github.com>
2021-04-22 21:00:45 -0700

    Update vps-enable-ssh-login.sh

--- vps-enable-ssh-login.sh
+++ vps-enable-ssh-login.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cat <<EOF > /etc/ssh/sshd_config
-PermitRootLogin yes
-PasswordAuthentication yes
-EOF
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 service sshd restart
 
