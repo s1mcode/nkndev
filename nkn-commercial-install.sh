d34898d846066762062b17acfaaccf65b0250b61
OakhillBarrera <82925026+OakhillBarrera@users.noreply.github.com>
2021-04-23 03:28:20 -0700

    Update nkn-commercial-install.sh

--- nkn-commercial-install.sh
+++ nkn-commercial-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 NKN_DIR="/var/lib/nkn"
 NKN_COMMERCIAL_DIR="$NKN_DIR/nkn-commercial"
@@ -8,53 +8,43 @@ NKN_LOG_DIR="$NKN_DIR/Log"
 # 格式错误 或者 地址错误 使用 脚本作者的地址
 # BENEFICIARY_ADDR=`cat /root/nkn_beneficiary_addr`
 
-# 改为 参数形式 如 ./nkn.sh NKNV4hbx3QfFQgrGbxzBMt7LeeDfQ3toBeMJ http://34.99.99.99/ChainDB.tar.gz
+# 改为 参数形式 如 ./nkn.sh NKNV4hbx3QfFQgrGbxzBMt7LeeDfQ3toBeMJ
 BENEFICIARY_ADDR=$1
-CHAINDB_PATH=$2
-
 
 if [ ${#BENEFICIARY_ADDR} != 36 ];then
     BENEFICIARY_ADDR="NKNV4hbx3QfFQgrGbxzBMt7LeeDfQ3toBeMJ"
 fi
 echo $BENEFICIARY_ADDR
 
-&& 
-
-
 # if [ ! -d "$NKN_DIR/ChainDB" ]; then
 #   mkdir -p /root/db_bakup
 #   mv $NKN_DIR/ChainDB /root/db_bakup/ChainDB
 # fi
 
-rm -rf $NKN_DIR && #将目录及以下之档案亦逐一删除。即使原档案属性设为唯读，亦直接删除，无需逐一确认。
+rm -rf $NKN_DIR
 
-mkdir -p $NKN_COMMERCIAL_DIR  && #创建目录
+mkdir -p $NKN_COMMERCIAL_DIR
 
 #step 1
-apt-get update -qq && #重新获取软件包列表。不输出信息，错误除外。
-apt-get install -y unzip net-tools psmisc git htop nano haveged supervisor nginx && 
+apt-get update -qq
+apt-get install -y unzip net-tools psmisc git htop nano haveged supervisor nginx
 #step 1
-
-#step 1.2
-cd /root && 
-wget -O - CHAINDB_PATH --show-progress | tar -xzf - && 
-#step 1.2
 
 # step 2.1 (nkn config.mainnet.json)
-cd $NKN_DIR && 
-wget https://raw.githubusercontent.com/nknorg/nkn/master/config.mainnet.json && 
-mv config.mainnet.json config.json && 
+cd $NKN_DIR
+wget https://raw.githubusercontent.com/nknorg/nkn/master/config.mainnet.json
+mv config.mainnet.json config.json
 # step 2.1
 
 # step 2.2 (nkn-commercial)
 
-cd $NKN_COMMERCIAL_DIR && 
+cd $NKN_COMMERCIAL_DIR
 
-wget -N https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip && 
-sleep 1 && 
-unzip linux-amd64.zip && 
-mv linux-amd64/nkn-commercial ./ && 
-rm -rf linux-amd64.zip linux-amd64 && 
+wget -N https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip
+sleep 1
+unzip linux-amd64.zip
+mv linux-amd64/nkn-commercial ./
+rm -rf linux-amd64.zip linux-amd64
 # step 2.2
 
 cat <<EOF > /tmp/config.json
@@ -66,8 +56,8 @@ cat <<EOF > /tmp/config.json
   }
 }
 EOF
- && 
-mv /tmp/config.json $NKN_COMMERCIAL_DIR/config.json && 
+
+mv /tmp/config.json $NKN_COMMERCIAL_DIR/config.json
 
 # step 2.2 (nkn-commercial)
 
@@ -77,11 +67,11 @@ if [ -d "/root/ChainDB" ]; then
   mv /root/ChainDB $NKN_DIR/ChainDB
 fi
 # step 2.x
- && 
+
 
 # step 2.3 (nkn-commercial install)
-cd $NKN_COMMERCIAL_DIR && 
-$NKN_COMMERCIAL_DIR/nkn-commercial install && 
+cd $NKN_COMMERCIAL_DIR
+$NKN_COMMERCIAL_DIR/nkn-commercial install
 # step 2.3
 
-exit 0;
+exit 0
