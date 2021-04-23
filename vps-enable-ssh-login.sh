787ab84acd8351aa935f65791e66536e58ee8e51
OakhillBarrera <82925026+OakhillBarrera@users.noreply.github.com>
2021-04-23 03:26:36 -0700

    Delete vps-enable-ssh-login.sh

--- vps-enable-ssh-login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-
-service sshd restart
-
-exit 0
-
